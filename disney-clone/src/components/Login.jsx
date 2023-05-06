fe8188f619201cf2f9059141c00f4b1f8cb64b63
ovie-davet <oviedavid77@mail.com>
2023-05-06 22:20:16 +0100

    Added tailwind css and styled component dependencies

---
+++ disney-clone/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Login = (props) => {
+
+  return (
+   <container className="">
+        <content>Hello</content>
+   </container>
+  )
+  
+}
+
+export default Login
