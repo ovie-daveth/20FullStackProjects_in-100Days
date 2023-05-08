ccd79e43d5820b5f1ffd78c2bdec1f081bb30436
ovie-davet <oviedavid77@mail.com>
2023-05-09 00:20:29 +0100

    Viewer.jsx: Continuation of the homePage UI

---
+++ disney-clone/frontend/src/components/Viewers.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import view1 from '../assets/images/viewers-disney.png'
+import view2 from '../assets/images/viewers-marvel.png'
+import view3 from '../assets/images/viewers-national.png'
+import view4 from '../assets/images/viewers-pixar.png'
+import view5 from '../assets/images/viewers-starwars.png'
+import vid1 from "../assets/videos/disney.mp4"
+import vid2 from "../assets/videos/marvel.mp4"
+import vid3 from "../assets/videos/geographic.mp4"
+import vid4 from "../assets/videos/pixar.mp4"
+import vid5 from "../assets/videos/star-wars.mp4"
+
+const Viewers = () => {
+    const vids = [vid1, vid2, vid3, vid4, vid5]
+    const views = [view1, view2, view3, view4, view5]
+  return (
+    <div className='grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 mt-2 gap-2 px-6'>
+        {views.map((view, index) => (
+        <div key={index} className="block relative group border-[2px] border-gray-500 rounded-md">
+          <img src={view} alt={`view${index}`} />
+          <video autoPlay muted loop className='group-hover:block hidden absolute top-0 opacity-80'>
+            <source src={vids[index]} type="video/mp4" />
+          </video>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Viewers
--- disney-clone/frontend/src/pages/Home.jsx
+++ disney-clone/frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import homebg from "../assets/images/home-background.png"
 import ImageSlider from '../components/ImageSlider'
+import Viewers from '../components/Viewers';
 
 const Home = () => {
   return (
     <div className="mt-16 overflow-hidden">
       <ImageSlider />
+      <Viewers />
       <div className="-z-10 absolute top-16">
         <img src={homebg} alt="bg" className="w-[100vw] h-screen" />
       </div>
